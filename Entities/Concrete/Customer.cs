5df572ed7d5000a20dd05ebc97367b0ef13e3337
gorkemakpur <gorkemakpur@gmail.com>
2023-11-13 00:40:00 +0300

    generic constraint update

---
+++ Entities/Concrete/Customer.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Customer : IEntity
+    {
+        //northwind normalize olmadığı için bu şekilde tutuluyor id 
+        public string CustomerId { get; set; }
+        public string ContactName { get; set; }
+        public string CompanyName { get; set; }
+        public string City { get; set; }
+    }
+}
