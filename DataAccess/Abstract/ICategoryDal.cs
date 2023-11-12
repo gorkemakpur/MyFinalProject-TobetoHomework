172d36b2ed8a6fe1b2d8b22daf828f0357dda2fd
gorkemakpur <gorkemakpur@gmail.com>
2023-11-12 23:25:42 +0300

    DAL->Generic Repository Added

--- DataAccess/Abstract/ICategoryDal.cs
+++ DataAccess/Abstract/ICategoryDal.cs
@@ -7,12 +7,8 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Abstract
 {
-    public interface ICategoryDal
+    public interface ICategoryDal : IEntityRepository<Category>
     {
-        List<Product> GetAll();
-        void Add(Product product);
-        void Update(Product product);
-        void Delete(Product product);
-        List<Product> GetAllByCategory(int categoryId);
+
     }
 }
