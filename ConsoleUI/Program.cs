4c3a3e93930327a2e1ba81ea56048eed073642da
gorkemakpur <gorkemakpur@gmail.com>
2023-11-16 02:07:42 +0300

    Dto Added

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -8,20 +8,47 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            ProductManager pm = new ProductManager(new EfProductDal());
-           /*
-            foreach (var item in pm.GetAllByCategoryId(2))
+            //ProductTest();
+            //CategoryTest();
+            //ProductTestV2();
+
+        }
+
+        private static void ProductTestV2()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var item in productManager.GetProductDetails())
             {
-                Console.WriteLine(item.ProductName);
+                Console.WriteLine(item.ProductName + "//" + item.CategoryName);
             }
-           */
-            foreach (var item in pm.GetByUnitPrice(10,20))
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+
+            foreach (var item in categoryManager.GetAll())
+            {
+                Console.WriteLine(item.CategoryName);
+
+            }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager pm = new ProductManager(new EfProductDal());
+            /*
+             foreach (var item in pm.GetAllByCategoryId(2))
+             {
+                 Console.WriteLine(item.ProductName);
+             }
+            */
+            foreach (var item in pm.GetByUnitPrice(10, 20))
             {
                 Console.WriteLine(item.ProductName);
                 Console.WriteLine(item.CategoryId);
             }
-
-
         }
     }
 }
